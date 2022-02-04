a247e1b7cc6473ac4ad164195dd7446ec19d9c55
cmolinaord <carlosmolina.ord@gmail.com>
2022-02-04 14:42:46 +0100

    Only apply change if panel is in other monitor when single screen

--- panel2-switch.sh
+++ panel2-switch.sh
@@ -4,21 +4,23 @@
 #
 # Carlos Molina (Jan 2022)
 
+# Detect panel2 position
+curr=$(xfconf-query -c xfce4-panel -v -p /panels/panel-2/output-name)
 # Detect monitors
 Ndisp=$(xrandr --listmonitors | head -n1 | cut -d' ' -f2)
 
 D1=$(xrandr --listmonitors | sed -n '2 p' | cut -d' ' -f6)
 if [ $Ndisp == 1 ]; then
 	# Single screen
-	notify-send "XFCE Panel" "Setting panel2 to single monitor ($D1)"
-	xfconf-query -c xfce4-panel -p /panels/panel-2/output-name -s $D1
-	
+	if [ $curr != $D1 ]; then
+		notify-send "XFCE Panel" "Setting panel2 to single monitor ($D1)"
+		xfconf-query -c xfce4-panel -p /panels/panel-2/output-name -s $D1
+	fi
 elif [ $Ndisp == 2 ]; then
 	# Dual screen
 	D2=$(xrandr --listmonitors | sed -n '3 p' | cut -d' ' -f6)
 	
 	# Switch the panel from current to the other
-	curr=$(xfconf-query -c xfce4-panel -v -p /panels/panel-2/output-name)
 	if [ $curr = $D1 ]; then
 		xfconf-query -c xfce4-panel -p /panels/panel-2/output-name -s $D2
 		notify-send "XFCE Panel" "Setting panel2 to second monitor ($D2)"
